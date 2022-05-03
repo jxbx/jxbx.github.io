9ed70bfc7b3fac51708797aef9b7bfbd8313696c
jxbx <bailey.justin@gmail.com>
2022-05-03 14:04:37 +0100

    tidied up code; added colour picker for drop shadow; added blend modes selector

--- js/patternator1.js
+++ js/patternator1.js
@@ -1,9 +1,12 @@
-const gridItems = document.getElementsByClassName("grid-item");
+//keeping track of state for css transform and textShadow properties
 
 let itemAngles = [];
 let itemStretches = [];
+let itemDropShadows = "none";
 
-window.onload = function randomise(){
+//generating 30x30 character grid; default character is "I"
+
+window.onload = function generateCharacters(){
   for (let i=0; i<900; i++){
     itemAngles.push(0);
     itemStretches.push(20);
@@ -19,6 +22,12 @@ window.onload = function randomise(){
     }
 }
 
+const gridItems = document.getElementsByClassName("grid-item");
+
+//SLIDERS
+
+//thickness (weight) slider
+
 const sliderThickness = document.getElementById("thickness");
 const outputThickness = document.getElementById("thicknessValue");
 outputThickness.innerHTML = sliderThickness.value;
@@ -26,21 +35,25 @@ outputThickness.innerHTML = sliderThickness.value;
 sliderThickness.oninput = function() {
   outputThickness.innerHTML = this.value;
 
-  for (let i=0; i<gridItems.length; i++){
-    gridItems[i].style.fontWeight = this.value;
+  for (const item of gridItems){
+    item.style.fontWeight = this.value;
   }
 }
+
+//font size slider
 
 const sliderFontSize = document.getElementById("fontSize");
 const outputFontSize = document.getElementById("fontSizeValue");
 outputFontSize.innerHTML = sliderFontSize.value;
+
 sliderFontSize.oninput = function () {
   outputFontSize.innerHTML = this.value;
-
-  for (let i=0; i<gridItems.length; i++){
-    gridItems[i].style.fontSize = this.value +"pt";
+  for (const item of gridItems){
+    item.style.fontSize = this.value +"pt";
   }
 }
+
+//jitter slider
 
 const sliderJitter = document.getElementById("jitter");
 const outputJitter = document.getElementById("jitterValue");
@@ -48,15 +61,18 @@ outputJitter.innerHTML = sliderJitter.value;
 
 sliderJitter.oninput = function() {
   outputJitter.innerHTML = this.value;
-  for (let i=0; i<gridItems.length; i++){
+  for (const item of gridItems){
 
     const randomMultiplier = function() {
       let plusOrMinus = Math.random() > 0.5 ? 1 : -1;
       return 0.5 * Math.random() * plusOrMinus;
     }
-    gridItems[i].style.margin = "0 0" + this.value*randomMultiplier() + "px " + this.value*randomMultiplier() + "px";
+
+    item.style.margin = "0 0" + this.value*randomMultiplier() + "px " + this.value*randomMultiplier() + "px";
   }
 }
+
+//drop shadow slider
 
 const sliderDropShadow = document.getElementById("dropShadow");
 const outputDropShadow = document.getElementById("dropShadowValue");
@@ -65,17 +81,20 @@ outputDropShadow.innerHTML = sliderDropShadow.value;
 sliderDropShadow.oninput = function () {
   outputDropShadow.innerHTML = this.value;
   if (this.value <=0){
-    for (let i=0; i<gridItems.length; i++){
-    gridItems[i].style.textShadow = "none";
+    for (const item of gridItems){
+      itemDropShadows = "none";
+      item.style.textShadow = itemDropShadows;
     }
   }
   else {
-    for (let i=0; i<gridItems.length; i++){
-    gridItems[i].style.textShadow = this.value + "px " + this.value + "px " + "0 " + "white";
+    itemDropShadows = this.value + "px " + this.value + "px " + "0 ";
+    for (const item of gridItems){
+      item.style.textShadow = itemDropShadows + picker2.color.rgbaString;
     }
   }
-
 }
+
+//spacing slider (note: this adjusts the grid container rather than the grid elements)
 
 const sliderSpacing = document.getElementById("spacing");
 const outputSpacing = document.getElementById("spacingValue");
@@ -83,10 +102,10 @@ outputSpacing.innerHTML = sliderSpacing.value;
 
 sliderSpacing.oninput = function () {
   outputSpacing.innerHTML = this.value;
-
-    gridContainer.style.gridTemplate = "repeat(30, " +  this.value + "px) / repeat(30, " + this.value + "px)";
-
+  gridContainer.style.gridTemplate = "repeat(30, " +  this.value + "px) / repeat(30, " + this.value + "px)";
 }
+
+//angle slider (adjusts angles of individual characters)
 
 const sliderAngle = document.getElementById("angle");
 const outputAngle = document.getElementById("angleValue");
@@ -101,10 +120,11 @@ sliderAngle.oninput = function () {
   }
 }
 
+//stretch slider
+
 const sliderStretch = document.getElementById("stretch");
 const outputStretch = document.getElementById("stretchValue");
 outputStretch.innerHTML = sliderStretch.value;
-
 
 sliderStretch.oninput = function () {
   outputStretch.innerHTML = this.value;
@@ -114,6 +134,7 @@ sliderStretch.oninput = function () {
   }
 }
 
+//rotation slider (adjusts angle of overall layout)
 
 const sliderRotation = document.getElementById("rotation");
 const outputRotation = document.getElementById("rotationValue");
@@ -124,7 +145,9 @@ sliderRotation.oninput = function () {
   gridContainer.style.transform = "rotate(" + this.value + "deg)";
 }
 
+//BUTTONS
 
+//randomise button (randomly adjusts the angle of each character)
 
 const randomiseButton = document.getElementById("randomiseButton");
 
@@ -137,6 +160,8 @@ randomiseButton.onclick = function () {
   }
 }
 
+//right angles button (randomly adjusts each character to 0 or 90 degrees)
+
 const rightAnglesButton = document.getElementById("rightAnglesButton");
 
 rightAnglesButton.onclick = function () {
@@ -147,10 +172,11 @@ rightAnglesButton.onclick = function () {
   }
 }
 
+//alternate button (adjusts character angles so they alternate between 90 and 270 degrees)
+
 const alternateButton = document.getElementById("alternateButton");
 
 alternateButton.onclick = function () {
-
   for (let i=0; i<gridItems.length; i++){
     if (i % 2 !==0){
       gridItems[i].style.transform = "rotate(90deg) scaleY(" + itemStretches[i]/20 + ")";
@@ -160,20 +186,23 @@ alternateButton.onclick = function () {
       gridItems[i].style.transform = "rotate(270deg) scaleY(" + itemStretches[i]/20 + ")";
       itemAngles[i] = 270;
     }
-
   }
 }
+
+//regularise button (adjusts all character angles to 0 degrees)
 
 const regulariseButton = document.getElementById("regulariseButton");
 
 regulariseButton.onclick = function () {
-
   for (let i=0; i<gridItems.length; i++){
     gridItems[i].style.transform = "rotate(0deg) scaleY(" + itemStretches[i]/20 + ")";
     itemAngles[i] = 0;
   }
-
 }
+
+//CHARACTER INPUT
+
+//input form which allows alternate characters to be inserted into the pattern
 
 const characterInput = document.getElementById("character");
 
@@ -183,6 +212,53 @@ characterInput.oninput = function () {
   }
 }
 
-function test () {
-  console.log(itemAngles);
+//BLEND MODE SELECTOR
+
+const blendOptions = document.getElementById("blendOptions");
+
+blendOptions.oninput = function () {
+  for (const item of gridItems){
+    item.style.mixBlendMode = blendOptions.value;
+  }
 }
+
+const parent0 = document.getElementById('parent0');
+const background = document.getElementById('wrapper');
+
+const picker0 = new Picker({
+  parent: parent0,
+  color: "#a7bec2ff",
+  popup: "top",
+  alpha: false,
+  onChange: function (color) {
+    background.style.backgroundColor = color.rgbaString;
+    parent0.style.backgroundColor = color.rgbaString;
+    }
+  });
+
+const parent1 = document.getElementById("parent1");
+const picker1 = new Picker({
+  parent: parent1,
+  color: "#947567ff",
+  popup: "top",
+  onChange: function (color) {
+    background.style.color = color.rgbaString;
+    parent1.style.backgroundColor = color.rgbaString;
+    }
+  });
+
+  const parent2 = document.getElementById("parent2");
+  const picker2 = new Picker({
+    parent: parent2,
+    color: "white",
+    popup: "top",
+    onChange: function (color) {
+      for (const item of gridItems){
+          item.style.textShadow = itemDropShadows + color.rgbaString;
+      }
+
+      parent2.style.backgroundColor = color.rgbaString;
+      }
+    });
+
+console.log(picker1.color)
