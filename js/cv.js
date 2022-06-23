8d0a59873a9320dcfae14514688cfc00343ce6b4
jxbx <bailey.justin@gmail.com>
2022-06-23 19:13:59 +0100

    updated stylesheet

--- js/cv.js
+++ js/cv.js
@@ -22,6 +22,12 @@ const education = [
     description: null,
   },
   {
+    course: "CELTA (certificate in English Language Teaching to Adults)",
+    institution: "Ealing, Hammersmith and West London College",
+    dates: "2014",
+    description: `120-hour English-teaching course including 6 hours of assessed teaching.`
+  },
+  {
     course: "MA in Film and Literature",
     institution: "University of York",
     dates: "2009 - 2011",
@@ -45,36 +51,51 @@ const work = [
   {
     employer: "Costello Medical",
     role: "Graphic designer",
-    dates: "August 2021 - May 2022",
-    description: null
+    dates: "Aug 2021 - May 2022",
+    url: "https://www.costellomedical.com",
+    description: `A wide-ranging graphic design and digital design role at an agency with clients in the medical sector, ranging from large corporate entities to small third sector and charity organisations. My responsibilities included:
+
+<ul>
+  <li>Supporting the development of websites for charities using the WordPress and Wix platforms through user testing and consulting on accessibility</li>
+  <li>Building, testing and deploying HTML emails via the Pure360 platform</li>
+  <li>Designing printed policy reports and factsheets for both medical specialist and general audiences</li>
+  <li>Designing technical reference materials for clinicians in interactive pdf and printed poster format</li>
+  <li>Developing new best practice accessibility guidelines for digital design and exhibition design</li>
+</ul>`
   },
   {
     employer: "The Charterhouse",
-    role: "visitor assistant",
-    dates: "June 2021 - August 2022",
-    description: null
+    role: "Visitor assistant",
+    dates: "Jun 2021 - Aug 2022",
+    url: "https://thecharterhouse.org",
+    description: `A part-time role based at a historic venue in Islington. I managed the venue's online
+retail and ticketing system using the DigiTickets platform, planned the delivery of guided tours and managed day-to-day operations.`
   },
   {
     employer: "St John Ambulance",
     role: "Event planning and delivery coordinator",
     dates: "April - December 2020",
+    url: "https://www.sja.org.uk",
     description: `Based at St John Ambulance's regional operations command centre. I coordinated the daily deployment of volunteer healthcare workers in response to Covid-19, managing the booking of healthcare volunteers onto hospital shifts and ambulance shifts and liaising with commercial clients to organise first aid support for behind closed doors sport events.`
   },
   {
     employer: "Museum of the Order of St John",
     role: "Lead designer and administrator",
     dates: "2017 - 2020",
+    url: "https://museumstjohn.org.uk",
     description: `A design role based at a historic venue in central London. My responsibilities included:
-Designing exhibitions, gallery displays and wayfinding signage based on best practice accesibility guidelines.
-Delivering marketing creative for commercial events and generating retail design concepts based on the
-museum collections.
-Managing financial administration of the museum and developing new public programme content including
-talks, tours and activity sessions`
+    <ul>
+<li>Designing exhibitions, gallery displays and wayfinding signage based on best practice accesibility guidelines</li>
+<li>Delivering marketing creative for commercial events and generating retail design concepts based on the museum collections</li>
+<li>Managing the museum's WordPress website, including updating content via the CMS and liaising with a digital agency to plan and implement design and functionality updates</li>
+<li>Supporting the development of public programme content including talks, tours and activity sessions</li>
+  </ul>`
   },
   {
     employer: "Museum of London",
     role: "Host and guide",
     dates: "2015 - 2017",
+    url: "https://www.museumoflondon.org.uk/museum-london",
     description: `Developed and delivered public engagement content including talks and tours and creative workshops across two sites at one of London's busiest museums.`
   }
 ]
@@ -83,10 +104,14 @@ window.onload = function () {
   for (const item of education){
     const newItem = document.createElement("div");
     newItem.setAttribute("class", "cvEntry");
+    const newDates = document.createElement("p");
     const newDetails = document.createElement("p");
     const newDescription = document.createElement("p");
-    newDetails.innerText = item.dates + " • " + item.course + " • " + item.institution;
+    newDates.setAttribute("class", "cvDate");
+    newDates.innerText = item.dates;
+    newDetails.innerText = item.course + " • " + item.institution
     newDescription.innerText = item.description;
+    newItem.appendChild(newDates);
     newItem.appendChild(newDetails);
     newItem.appendChild(newDescription);
     cvEducationList.appendChild(newItem);
@@ -95,13 +120,16 @@ window.onload = function () {
   for (const item of work){
     const newItem = document.createElement("div");
     newItem.setAttribute("class", "cvEntry");
+    const newDates = document.createElement("p");
     const newDetails = document.createElement("p");
     const newDescription = document.createElement("p");
-    newDetails.innerText = item.dates + " • " + item.employer + " • " + item.role;
-    newDescription.innerText = item.description;
+    newDates.setAttribute("class", "cvDate");
+    newDates.innerText = item.dates;
+    newDetails.innerHTML = "<a href=\"" + item.url + "\">" + item.employer + "</a>" + " • " + item.role;
+    newDescription.innerHTML = item.description;
+    newItem.appendChild(newDates);
     newItem.appendChild(newDetails);
     newItem.appendChild(newDescription);
     cvWorkList.appendChild(newItem);
-
   }
 }
